190bebab7bb8643a4f40c0e9d11f33e7b752e972
Дом <rsdev.info@gmail.com>
2016-01-12 23:29:40 +0700

    12.01 3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,6 +9,8 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.SessionsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 import servlets.UsersServlet;
 
 /**
@@ -28,6 +30,12 @@ public class Main {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        //мои сервлеты
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+
+
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setResourceBase("public_html");
---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,53 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Дом on 12.01.2016.
+ */
+public class SignUpServlet extends HttpServlet {
+
+    private final AccountService accountService;
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String pass = request.getParameter("pass");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        UserProfile profile = accountService.getUserByLogin(login);
+        //Проверка на существование пользователя в карте для регистрации
+        if (profile != null){
+            return;
+        }
+        else {
+            accountService.addNewUser(profile);
+        }
+
+
+
+
+    }
+
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
